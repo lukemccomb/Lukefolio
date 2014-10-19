2e8434ac7976172a85e33f606a67a51622bafa50
Luke McComb <lucasmccomb@gmail.com>
2014-10-19 11:25:15 -0600

    user can edit their user info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,24 @@
 class UsersController < ApplicationController
+
   def show
     @user = current_user
   end
-  def index
 
+  def edit
+    @user = current_user
   end
+
+  def update
+    user = current_user
+    user.update_attributes(allowed_parameters)
+    user.save
+    redirect_to user_path(current_user.id)
+  end
+
+  private
+
+  def allowed_parameters
+    params.require(:user).permit(:name, :email, :password)
+  end
+
 end
