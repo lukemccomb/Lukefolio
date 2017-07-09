70b5518b081337b99319d2eabf067c953ba9db7e
Luke McComb <lemccomb@us.ibm.com>
2017-07-08 23:45:28 -0600

    Adds heroku config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.web_console.development_only = false
 end
